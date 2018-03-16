90a637c082abb4f0a1d4124b9d1c3442ccd450e5
adrianwit <adrianwit@gmail.com>
2018-03-15 20:07:02 -0700

    added app

---
+++ app.go
@@ -0,0 +1,31 @@
+package echo
+
+import (
+	"net/http"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"log"
+)
+
+
+
+//simple echo app
+func main() {
+	port := os.Args[1];
+	if port == "" {
+		log.Fatal("usage: echo port")
+	}
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(http.StatusOK)
+		if request.ContentLength > 0 {
+			if content, err := ioutil.ReadAll(request.Body);err == nil {
+				if len(content) > 0 {
+					writer.Write(content)
+				}
+			}
+		}
+	})
+	fmt.Printf("Starting echo app om port " + port + "\n")
+	http.ListenAndServe(":" + port, nil)
+}
